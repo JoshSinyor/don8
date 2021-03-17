f232f3959d0473c141a580889cb481f7bd9a62ff
Oscar Bertrand <oscarb1989@yahoo.co.uk>
2021-03-17 17:05:55 +0000

    Creating first model test

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const bodyParser = require('body-parser')
-const morgan = require('morgan')
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
 //const Ad = require('./models/ad') ? if we need this
 require("dotenv/config");
 
