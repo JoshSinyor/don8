73f7a5be6cd73f6f825475a4b96a090380ee6a5c
hacaravan <hacavanagh@gmail.com>
2021-03-17 15:19:34 +0000

    Adding initial styling to homepage

--- Components/Ad.js
+++ Components/Ad.js
@@ -3,11 +3,23 @@ import { Button, StyleSheet, Text, View } from 'react-native';
 
 const Ad = (props) => {
   return(
-    <View>
+    <View style={[styles.item, { margin: 8, padding: 8}]}>
       <Text> {props.name} </Text>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  item: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    borderColor: '#69f',
+    borderWidth: 1,
+    borderRadius: 5,
+    backgroundColor: 'whitesmoke'
+  }
+})
 
 
 export default Ad;
--- Components/Home.js
+++ Components/Home.js
@@ -7,13 +7,23 @@ const Home = () => {
 
     const [title, setTitle] = useState('Don8')
     return(
-        <View>
-            <Text> {title} </Text>
+        <View style={{ width: '80%', marginBottom: 60 }}>
+            <Text style={[styles.align, styles.font]}> {title} </Text>
+            <Button title='Make a new request' onPress={() => setTitle('Don8!')}/>
             <Ad name={'First Ad'}/>
             <Ad name={'Second Ad'}/>
-            <Button title='Make a new request' onPress={() => setTitle('Don8!')}/>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    align: {
+      alignSelf: 'center'
+    },
+    font: {
+      fontSize: 20,
+      fontWeight: 'bold'
+    }
+})
 
 export default Home;
