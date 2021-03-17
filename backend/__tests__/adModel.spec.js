839e23b4e4765987d5154ecffedc0527d9aa9977
Sean Edwards <seantrevoredwards@gmail.com>
2021-03-17 19:04:03 +0000

    Jest now working first test for adding an advert is complete

--- backend/__tests__/adModel.spec.js
+++ backend/__tests__/adModel.spec.js
@@ -1,21 +1,31 @@
 const express = require('express');
-const adsModel = require('../models/ad');
-const dbHandler = require('db-handler');
+const { Ad } = require('../models/ad');
+const dbHandler = require('../db-handler');
+require("dotenv").config();
+
 // const request = require('supertest');
-// const app = express();
-// const api = process.env.API_URL;
-const app = require("../app");
 const mongoose = require("mongoose");
-const supertest = require("supertest");
+const charityAd = { title: 'Oxfam', location: 'London', description: 'We need help', contact: 'test@test.com' };
 
 beforeAll(async () => await dbConnect());
+afterEach(async () => await dbClear());
+afterAll(async () => await dbClose());
 
-describe("Ad",function() {
-  describe("Create", function () {
-    it("charity can post an ad", function() {
+describe("Ad",() => {
+  describe("Create", () => {
 
+    it("charity can post an ad", async () => {
+      const validAd = new Ad(charityAd);
+      const savedAd = await validAd.save();
+      // Object Id should be defined when successfully saved to MongoDB.
+      expect(savedAd._id).toBeDefined();
+      expect(savedAd.title).toBe(charityAd.title);
+      expect(savedAd.location).toBe(charityAd.location);
+      expect(savedAd.description).toBe(charityAd.description);
+      expect(savedAd.contact).toBe(charityAd.contact);
     });
   });
 });
 
-module.exports = app
+
+// module.exports = app
