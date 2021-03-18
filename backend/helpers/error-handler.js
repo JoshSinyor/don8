af2178948c6e683411b174bdef21d24b4820d699
Sean Edwards <seantrevoredwards@gmail.com>
2021-03-18 22:13:27 +0000

    Added comments in the error helper

--- backend/helpers/error-handler.js
+++ backend/helpers/error-handler.js
@@ -1,12 +1,14 @@
 function errorHandler(err, req, res, next) {
     if (err.name === 'UnauthorizedError') {
+        // jwt authentication error
         return res.status(401).json({message: "The user is not authorized"})
     }
 
     if (err.name === 'ValidationError') {
         return res.status(401).json({message: err})
     }
-
+    
+    // default to 500 server code
     return res.status(500).json(err)
 };
 
