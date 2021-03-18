c40b815ff68f19b09761621312bf7643f96cc244
hacaravan <hacavanagh@gmail.com>
2021-03-18 15:14:30 +0000

    Styling navbar

--- App.js
+++ App.js
@@ -5,11 +5,15 @@ import Home from './Components/Home';
 // import AdContainer from './Screens/Ads/AdContainer'
 import Navbar from './Components/Navbar'
 
+import './node_modules/bootstrap/dist/css/bootstrap.min.css';
+// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import NavigationBar from './Components/Navbar';
+
 export default function App() {
   return (
     <>
      <View style={styles.navbar}>
-        <Navbar />
+        <NavigationBar />
       </View>
       <View style={styles.container}>
         < Home />
@@ -22,11 +26,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  navbar: {
-    backgroundColor: 'yellowgreen',
     alignItems: 'center',
     justifyContent: 'center',
   },
--- Components/Navbar.js
+++ Components/Navbar.js
@@ -1,23 +1,66 @@
 import React from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Nav, Navbar} from 'react-bootstrap';
+// , Form, FormControl
+import styled from 'styled-components';
 
-const Navbar = () => {
-  return (
-    <Text>This is a Navbar</Text>
-  )
-}
-
-const styles = StyleSheet.create({
-  item: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    borderColor: '#69f',
-    borderWidth: 1,
-    borderRadius: 5,
-    backgroundColor: 'whitesmoke'
+const Styles = styled.div`
+  .navbar {
+    background-color: #fff;
+    border-bottom: black solid 1px;
   }
-})
+  a, .navbar-nav, .navbar-light .nav-link {
+    color: #000;
+    &:hover { color: white; }
+  }
+  .navbar-brand {
+    font-size: 1.4em;
+    background-color: pink;
+    color: #000;
+    &:hover { color: grey; }
+  }
+  .form-center {
+    position: absolute !important;
+    left: 25%;
+    right: 25%;
+  }
+`;
 
+const NavigationBar = () => (
+  <Styles>
+    <Navbar expand="lg">
+      <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ml-auto">
+          <Nav.Item><Nav.Link href="/">Home</Nav.Link></Nav.Item>
+        </Nav>
+      </Navbar.Collapse>
+      <Navbar.Brand href="/">don8</Navbar.Brand>
 
-export default Navbar;
+      {// <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+      // <Form className="form-center">
+      //   <FormControl type="text" placeholder="Search" className="" />
+      // </Form>
+      // <Navbar.Collapse id="basic-navbar-nav">
+      //   <Nav className="ml-auto">
+      //     <Nav.Item><Nav.Link href="/">Home</Nav.Link></Nav.Item>
+      //     // <Nav.Item><Nav.Link href="/about">About</Nav.Link></Nav.Item>
+      //   </Nav>
+      // </Navbar.Collapse>
+    }
+    </Navbar>
+  </Styles>
+)
+
+// const styles = StyleSheet.create({
+//   item: {
+//     flexDirection: 'row',
+//     justifyContent: 'space-between',
+//     alignItems: 'center',
+//     borderColor: '#69f',
+//     borderWidth: 1,
+//     borderRadius: 5,
+//     backgroundColor: 'whitesmoke'
+//   }
+// })
+
+export default NavigationBar
