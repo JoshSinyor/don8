4b2c2e31d035a6bdbac9a5c566ec75dcbd390071
hacaravan <hacavanagh@gmail.com>
2021-03-18 14:34:00 +0000

    Changing component to capital C in App.js imports

--- App.js
+++ App.js
@@ -1,9 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Home from './components/Home';
+import Home from './Components/Home';
 // import AdContainer from './Screens/Ads/AdContainer'
-import Navbar from './components/Navbar'
+import Navbar from './Components/Navbar'
 
 export default function App() {
   return (
