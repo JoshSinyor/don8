5e618d36c52de383d7693295427a03593b1094d5
hacaravan <hacavanagh@gmail.com>
2021-03-19 12:29:54 +0000

    Moving unnecessary code out of app.js

--- backend/app.js
+++ backend/app.js
@@ -1,74 +1,22 @@
-const express = require("express");
-const app = express();
+// Connect to database
 const mongoose = require("mongoose");
+require("dotenv").config();
+// Connect to port
+const app = require("./server");
 
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
-const cors = require('cors');
-const authJwt = require('./helpers/jwt')
-const errorHandler = require('./helpers/error-handler')
-
-require("dotenv/config");
-
-app.use(cors());
-app.options('*', cors());
-
-// middleware
-app.use(bodyParser.json());
-app.use(morgan('tiny'));
-
-app.use(authJwt());
-app.use(errorHandler);
-
-// routes
-const adsRouter = require('./routes/ads')
-const usersRouter = require('./routes/users');
-
-const api = process.env.API_URL;
-
-app.use(`${api}/ads`, adsRouter)
-app.use(`${api}/users`, usersRouter)
-
-// database connection
-mongoose
-  .connect(process.env.DATABASE_CONNECTION, {
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false
-  })
-  .then(() => {
-    console.log("Database connection is ready");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
-
-// server connection check
-app.listen(3000, () => {
-  console.log("Server is running on http://localhost:3000");
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
 
+const uri = process.env.DATABASE_CONNECTION;
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
 
-// // Connect to database
-// const mongoose = require("mongoose");
-// require("dotenv").config();
-// // Connect to port
-// const app = require("./server");
-
-// const port = process.env.PORT || 3000;
-// app.listen(port, () => {
-//   console.log(`Server is running on port ${port}`);
-// });
-
-// const uri = process.env.DATABASE_CONNECTION;
-// mongoose.connect(uri, {
-//   useNewUrlParser: true,
-//   useCreateIndex: true,
-//   useUnifiedTopology: true,
-// });
-
-// const connection = mongoose.connection;
-// connection.once("open", () => {
-//   console.log("MongoDB database connection established successfully");
-// });
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("MongoDB database connection established successfully");
+});
