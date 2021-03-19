21cb1ac2c696a3c3b7ab0f7f8c1c5060eda40e43
Sean Edwards <seantrevoredwards@gmail.com>
2021-03-19 10:00:16 +0000

    Added database split into app.js and server.js

--- backend/app.js
+++ backend/app.js
@@ -44,3 +44,27 @@ mongoose
 app.listen(3000, () => {
   console.log("Server is running on http://localhost:3000");
 });
+
+
+// // Connect to database
+// const mongoose = require("mongoose");
+// require("dotenv").config();
+// // Connect to port
+// const app = require("./server");
+
+// const port = process.env.PORT || 3000;
+// app.listen(port, () => {
+//   console.log(`Server is running on port ${port}`);
+// });
+
+// const uri = process.env.DATABASE_CONNECTION;
+// mongoose.connect(uri, {
+//   useNewUrlParser: true,
+//   useCreateIndex: true,
+//   useUnifiedTopology: true,
+// });
+
+// const connection = mongoose.connection;
+// connection.once("open", () => {
+//   console.log("MongoDB database connection established successfully");
+// });
