f026ac9286b44bd89bee0b198dc106879c95fcbb
Sean Edwards <seantrevoredwards@gmail.com>
2021-03-20 13:55:12 +0000

    Made header with either logo image or title Don8

--- App.js
+++ App.js
@@ -3,11 +3,13 @@ import React from 'react';
 import { StyleSheet, Text, View, FlatList, ActivityIndicator} from 'react-native';
 
 // screens
+import Header from './Shared/Header'
 import AdContainer from './Screens/Ads/AdContainer'
 
 export default function App() {
   return (
     <View style={styles.container}>
+        <Header />
         < AdContainer />
     </View>
   );
