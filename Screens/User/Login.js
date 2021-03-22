83bc4bd316593128b562bb0cee8de7d1f394ca6d
Sean Edwards <seantrevoredwards@gmail.com>
2021-03-22 21:17:17 +0000

    Added easybutton and error to shared folder

--- Screens/User/Login.js
+++ Screens/User/Login.js
@@ -1,7 +1,11 @@
 import React, { useEffect, useState } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Button } from "react-native";
 import FormContainer from "../../Shared/Form/FormContainer";
+import Input from "../../Shared/Form/Input";
+
 const Login = (props) => {
+  const [email, setEmail] = useState("");
+  const [password, setpassword] = useState("");
   return (
     <View>
       <Text>Login Screen</Text>
