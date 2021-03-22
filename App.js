39960accfb6ff2c9406573919d14f792d87b9ae4
Sean Edwards <seantrevoredwards@gmail.com>
2021-03-22 16:59:29 +0000

    map appeared for a very short time

--- App.js
+++ App.js
@@ -1,25 +1,47 @@
-import * as React from 'react';
-import MapView from 'react-native-maps';
-import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import * as React from "react";
+import MapView from "react-native-maps";
+import * as Permissions from "expo";
+import { StyleSheet, Text, View, Dimensions } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <MapView 
-        style={styles.map} />
-    </View>
-  );
+export default class App extends React.Component {
+  state = {
+    latitude: null,
+    longitude: null,
+  };
+  async componentDidMount() {
+    const { status } = await Permissions.getAsync(Permissions.LOCATION);
+
+    if (status !== "granted") {
+      const response = await Permissions.askAsync(Permissions.LOCATION);
+    }
+  }
+  render() {
+    const { latitude, longitude } = this.state;
+    return (
+      <View style={styles.container}>
+        <MapView
+          style={styles.map}
+          initialRegion={{
+            latitude,
+            longitude,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
   map: {
-    width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height,
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height,
   },
 });
