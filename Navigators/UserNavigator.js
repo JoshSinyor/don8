289d57ccc354e895795223120515064327641d25
Sean Edwards <seantrevoredwards@gmail.com>
2021-03-22 20:57:49 +0000

    Added stack screens for the users nav

--- Navigators/UserNavigator.js
+++ Navigators/UserNavigator.js
@@ -7,7 +7,35 @@ import UserProfile from "../Screens/User/UserProfile";
 
 const Stack = createStackNavigator();
 
-function MyStack() {}
+function MyStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Login"
+        component={Login}
+        options={{
+          headerShown: false,
+        }}
+      />
+
+      <Stack.Screen
+        name="Register"
+        component={Register}
+        options={{
+          headerShown: false,
+        }}
+      />
+
+      <Stack.Screen
+        name="UserProfile"
+        component={UserProfile}
+        options={{
+          headerShown: false,
+        }}
+      />
+    </Stack.Navigator>
+  );
+}
 
 export default function UserNavigator() {
   return <MyStack />;
