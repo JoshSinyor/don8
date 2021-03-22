c98c95d87d3c22fba9a1e5e9787a0d6745cdfcc2
Sean Edwards <seantrevoredwards@gmail.com>
2021-03-22 20:50:46 +0000

    Added screens for users

---
+++ Screens/User/Login.js
@@ -0,0 +1,12 @@
+import React, from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const Login = (props) => {
+    return (
+        <View>
+            <Text>Login Screen</Text>
+        </View>
+    )
+}
+
+export default Login;
---
+++ Screens/User/UserProfile.js
@@ -0,0 +1,12 @@
+import React, from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const UserProfile = (props) => {
+    return (
+        <View>
+            <Text>User profile Screen</Text>
+        </View>
+    )
+}
+
+export default UserProfile;
