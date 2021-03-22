58a34ae5901c31714ccd33580acdcded9df20029
Oscar Bertrand <oscarb1989@yahoo.co.uk>
2021-03-22 12:17:43 +0000

    Installed react native maps -  trying to render map but does not work yet

--- App.js
+++ App.js
@@ -1,22 +1,35 @@
-import { StatusBar } from "expo-status-bar";
+// import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { LogBox } from "react-native";
-import { NavigationContainer } from "@react-navigation/native";
-
+import { StyleSheet, Text, View } from 'react-native';
+// import { LogBox } from "react-native";
+// import { NavigationContainer } from "@react-navigation/native";
+import MapView from 'expo';
 // Navigators
-import Main from "./Navigators/Main";
+// import Main from "./Navigators/Main";
 
 // screens
-import Header from "./Shared/Header";
-import AdContainer from "./Screens/Ads/AdContainer";
+// import Header from "./Shared/Header";
+// import AdContainer from "./Screens/Ads/AdContainer";
 
-LogBox.ignoreAllLogs(true);
+// LogBox.ignoreAllLogs(true);
 
-export default function App() {
-  return (
-    <NavigationContainer>
-      <Header />
-      <Main />
-    </NavigationContainer>
-  );
+export default class App extends React.Component {
+//   state = {
+//     latitude: null,
+//     longitude: null
+//   }
+  
+//   async componentDidMount () {
+//   const { status } = await Permissions.getAsync(Permissions.LOCATION)
+
+//   if (status != 'granted') {
+//     const response = await Permissions.askAsync(Permissions.LOCATION)
+//   }
+// }
+
+  render() {
+    return (
+      <MapView></MapView>
+    );
+  }
 }
