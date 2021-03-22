beb3ebb874f514c639a6e0bc6a45f348edf1fba3
Sean Edwards <seantrevoredwards@gmail.com>
2021-03-22 15:08:15 +0000

    Added ability to upload images to database and render them in the localhost

--- backend/routes/ads.js
+++ backend/routes/ads.js
@@ -1,56 +1,88 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const {Ad} = require('../models/ad');
+const { Ad } = require("../models/ad");
+const multer = require("multer");
 
+const FILE_TYPE_MAP = {
+  "image/png": "png",
+  "image/jpeg": "jpeg",
+  "image/jpg": "jpg",
+};
 
-router.get('/', async (req, res) => {
-    const adList = await Ad.find();
-  
-    if(!adList) {
-      res.status(500).json({success: false})
-    } 
-    res.send(adList);
-  });
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    const isValid = FILE_TYPE_MAP[file.mimetype];
+    let uploadError = new Error("invalid image type");
 
-  router.get('/:id', async (req, res) => {
-    const ad = await Ad.findById(req.params.id);
-  
-    if(!ad) {
-      res.status(500).json({success: false})
+    if (isValid) {
+      uploadError = null;
     }
-    res.send(ad);
-  });
-  
-router.post('/', (req, res) => {
-    const ad = new Ad({
-      title: req.body.title,
-      location: req.body.location,
-      description: req.body.description,
-      contact: req.body.contact,
-      charity: req.body.charity,
+    cb(uploadError, "public/uploads");
+  },
+  filename: function (req, file, cb) {
+    const fileName = file.originalname.split(" ").join("-");
+    const extension = FILE_TYPE_MAP[file.mimetype];
+    cb(null, `${fileName}-${Date.now()}.${extension}`);
+  },
+});
 
+const uploadOptions = multer({ storage: storage });
+
+router.get("/", async (req, res) => {
+  const adList = await Ad.find();
+
+  if (!adList) {
+    res.status(500).json({ success: false });
+  }
+  res.send(adList);
+});
+
+router.get("/:id", async (req, res) => {
+  const ad = await Ad.findById(req.params.id);
+
+  if (!ad) {
+    res.status(500).json({ success: false });
+  }
+  res.send(ad);
+});
+
+router.post("/", uploadOptions.single("image"), (req, res) => {
+  // const file = req.file; if we want all posts to have images
+  // if (!file) return res.status(400).send("No image in the request");
+
+  const fileName = file.filename;
+  const basePath = `${req.protocol}://${req.get("host")}/public/uploads/`;
+  const ad = new Ad({
+    title: req.body.title,
+    location: req.body.location,
+    description: req.body.description,
+    contact: req.body.contact,
+    charity: req.body.charity,
+    image: `${basePath}${fileName}`,
+    website: req.body.website,
+  });
+
+  ad.save()
+    .then((createdAd) => {
+      res.status(201).json(createdAd);
     })
-  
-    ad.save().then((createdAd => {
-      res.status(201).json(createdAd)
-    })).catch((err => {
+    .catch((err) => {
       res.status(500).json({
         error: err,
-        success: false
-      })
-    }))
-  });
-
-  router.get('/get/count', async (req, res) => {
-    const adCount = await Ad.countDocuments((count) => count)
-  
-    if(!adCount) {
-      res.status(500).json({success: false})
-    }
-    res.send({
-      adCount: adCount
+        success: false,
+      });
     });
+});
+
+router.get("/get/count", async (req, res) => {
+  const adCount = await Ad.countDocuments((count) => count);
+
+  if (!adCount) {
+    res.status(500).json({ success: false });
+  }
+  res.send({
+    adCount: adCount,
   });
-  
-  
+});
+
 module.exports = router;
