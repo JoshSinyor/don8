b08d325840fe0f965179941be0417ac9a1b31e2e
Sean Edwards <seantrevoredwards@gmail.com>
2021-03-23 14:25:27 +0000

    Added Auth file and imported it into App.js

--- App.js
+++ App.js
@@ -3,6 +3,10 @@ import React from "react";
 import { LogBox } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import Toast from "react-native-toast-message";
+
+// context API
+import Auth from "./Context/Auth";
+
 // Navigators
 import Main from "./Navigators/Main";
 
@@ -14,10 +18,12 @@ LogBox.ignoreAllLogs(true);
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Header />
-      <Main />
-      <Toast ref={(ref) => Toast.setRef(ref)} />
-    </NavigationContainer>
+    <Auth>
+      <NavigationContainer>
+        <Header />
+        <Main />
+        <Toast ref={(ref) => Toast.setRef(ref)} />
+      </NavigationContainer>
+    </Auth>
   );
 }
