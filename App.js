d7ebf6d4943049be807914c87820f452bc5554f5
Oscar Bertrand <oscarb1989@yahoo.co.uk>
2021-03-23 11:53:14 +0000

    Set up google api - JSON response returns empty file

--- App.js
+++ App.js
@@ -1,60 +1,124 @@
-import * as React from "react";
-import MapView from "react-native-maps";
+
+import * as React from 'react'
+import { StyleSheet, View, Text } from 'react-native'
+import MapView from 'react-native-maps'
 import * as Permissions from 'expo-permissions';
-import { StyleSheet, Text, View, Dimensions } from "react-native";
+import Polyline from '@mapbox/polyline'
+import Constants from 'expo-constants';
+
+const locations = require('./location.json')
+
+// let googleApi = Constants.manifest.extra.googleApi
 
 export default class App extends React.Component {
   state = {
     latitude: null,
     longitude: null,
-  };
-  async componentDidMount() {
-    const { status } = await Permissions.getAsync(Permissions.LOCATION);
-
-    if (status !== "granted") {
-      const response = await Permissions.askAsync(Permissions.LOCATION);
-    }
-    navigator.geolocation.getCurrentPosition (
-      ({ coords: { latitude, longitude }}) => this.setState ({ latitude, longitude }, () => console.log('State:' , this.state)),
-      (error) => console.log('Error', error)
-    )
+    locations: locations
   }
-  render() {
-    const { latitude, longitude } = this.state;
-    
+
+  async componentDidMount() {
+    const { status } = await Permissions.getAsync(Permissions.LOCATION)
+
+    if (status !== 'granted') {
+      const response = await Permissions.askAsync(Permissions.LOCATION)
+    }
+
+    navigator.geolocation.getCurrentPosition(
+      ({ coords: { latitude, longitude } }) => this.setState({ latitude, longitude }, this.mergeCoords),
+      (error) => console.log('Error:', error)
+    )
+
+    const { locations: [ sampleLocation] } = this.state
+
+    this.setState({
+      desLatitude: sampleLocation.lat,
+      desLongitude: sampleLocation.lng
+    }, this.mergeCoords)
+  }
+  
+  mergeCoords = () => {
+    const {
+      latitude,
+      longitude,
+      desLatitude,
+      desLongitude
+    } = this.state
+
+    const hasStartAndEnd = latitude !== null && desLatitude !== null
+      if (hasStartAndEnd) {
+      const concatStart = `${latitude},${longitude}`
+      const concatEnd = `${desLatitude},${desLongitude}`
+      this.getDirections(concatStart, concatEnd)
+    }
+  }
+
+  // async getDirections(startLoc, desLoc) {
+  //   try {
+  //   const resp = await fetch(`https://maps.googleapis.com/maps/api/directions/json?origin=${startLoc}&destination=${desLoc}&key=AIzaSyBCO5_tSm-nS1221XjNP9tHVHc6HDDE4mw`)
+  //   const respJson = await resp.json();
+  //   console.log(respJson)
+  //   const points = Polyline.decode(respJson.routes[0].overview_polyline.points);
+  //   const coords = points.map(point => {
+  //     return {
+  //       latitude: point[0],
+  //       longitude: point[1]
+  //     }
+  //   })
+  //   this.setState({ coords })
+  //   } catch(error) {
+  //   console.log('Error: ', error)
+  //   }
+  // }
+
+  async getDirections(startLoc, destinationLoc) {
+    try {
+         const resp = await fetch(`https://maps.googleapis.com/maps/api/directions/json?origin=${startLoc}&destination=${destinationLoc}&key=AIzaSyBCO5_tSm-nS1221XjNP9tHVHc6HDDE4mw`);
+         const respJson = await resp.json();
+         console.log(respJson);
+         if (respJson.routes.length > 0) {
+             const points = Polyline.decode(respJson.routes[0].overview_polyline.points);
+             const coords = points.map((point, index) => {
+                 return {
+                     latitude: point[0],
+                     longitude: point[1],
+                 };
+             });
+             this.setState({ coords });
+         }
+         return;
+     } catch (error) {
+         alert(error);
+     }
+  }
+
+  render(){
+    const { latitude, longitude, coords } = this.state
     if (latitude) {
       return (
-        <View style={styles.container}>
-          <MapView
-            showsUserLocation
-            style={styles.map}
-            initialRegion={{
-              latitude,
-              longitude,
-              latitudeDelta: 0.0922,
-              longitudeDelta: 0.0421,
-            }}
-          />
-        </View>
-    );
-  }
-  return(
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>We need your permission!</Text>
-    </View>
-  )
+    <MapView
+      showsUserLocation
+      style={{ flex: 1 }}
+      initialRegion={{
+        latitude,
+        longitude,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421
+      }}
+      >
+      <MapView.Polyline 
+        strokeWidth={2}
+        strokeColor="red"
+        coordinates={coords}
+      />
+      
+    </MapView>
+      )
+    }
+    return (
+      <View style={{ flex: 1, justifyContent: 'center'}}>
+        <Text>We need your permission!</Text>
+      </View>
+    )
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  map: {
-    width: Dimensions.get("window").width,
-    height: Dimensions.get("window").height,
-  },
-});
---
+++ app.config.js
@@ -0,0 +1,8 @@
+// export default ({ config }) => {
+//   // config.extra.backendUrl = ‘http://192.168.1.169:3000’
+//   config.extra.googleApi = 'AIzaSyBCO5_tSm-nS1221XjNP9tHVHc6HDDE4mw'
+//   // config.ios.googleMapsApiKey = ‘AIzaSyAZ4FMOvT4DuSnNHPVBqQSYqoDaT8ScJz0’
+//   return {
+//     ...config,
+//   };
+// };
