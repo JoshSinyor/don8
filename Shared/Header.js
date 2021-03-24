aff71ce7397a974a1d0f12e409523f3a068e5d8b
Sean Edwards <seantrevoredwards@gmail.com>
2021-03-24 17:45:01 +0000

    Added image png to header

--- Shared/Header.js
+++ Shared/Header.js
@@ -3,25 +3,30 @@ import { StyleSheet, Image, Text, SafeAreaView, View } from "react-native";
 
 const Header = () => {
   return (
-      <SafeAreaView style={styles.header}>
-        <Text style={styles.title}>Don8</Text>
-      </SafeAreaView>
-    );
+    <SafeAreaView style={styles.header}>
+      {/* <Text style={styles.title}>Don8</Text> */}
+      <Image
+        source={require("../assets/icon.png")}
+        resizeMode="contain"
+        style={{ height: 50 }}
+      />
+    </SafeAreaView>
+  );
 };
 
 const styles = StyleSheet.create({
   header: {
-     width: "100%",
-     flexDirection: "row",
-     alignContent: "center",
-     justifyContent: "center",
-     padding: 20,
-   },
-   title: {
-     fontWeight: "bold",
-     fontSize: 50,
-     textAlign: "center",
-   },
+    width: "100%",
+    flexDirection: "row",
+    alignContent: "center",
+    justifyContent: "center",
+    padding: 20,
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 50,
+    textAlign: "center",
+  },
 });
 
 export default Header;
