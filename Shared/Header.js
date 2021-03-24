b37df91fcfab9123d30399e614367e1f1c6c8827
Joshua Sinyor <>
2021-03-24 11:00:20 +0000

    Update Header.js. Reflects return to text logo.

--- Shared/Header.js
+++ Shared/Header.js
@@ -3,23 +3,25 @@ import { StyleSheet, Image, Text, SafeAreaView, View } from "react-native";
 
 const Header = () => {
   return (
-    <SafeAreaView style={styles.header}>
-      <img src={require('../assets/logo_hourglass_rounded.svg')}
-          resizeMode="contain"
-          style={{ height: 50 }}
-      />
-    </SafeAreaView>
-  );
+      <SafeAreaView style={styles.header}>
+        <Text style={styles.title}>Don8</Text>
+      </SafeAreaView>
+    );
 };
 
 const styles = StyleSheet.create({
   header: {
-    width: "100%",
-    flexDirection: "row",
-    alignContent: "center",
-    justifyContent: "left",
-    margin: 20,
-  },
+     width: "100%",
+     flexDirection: "row",
+     alignContent: "center",
+     justifyContent: "center",
+     padding: 20,
+   },
+   title: {
+     fontWeight: "bold",
+     fontSize: 50,
+     textAlign: "center",
+   },
 });
 
 export default Header;
