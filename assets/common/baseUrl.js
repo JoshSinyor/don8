8b5ad0c918b0088386c5de2262fccac57476c98f
hacaravan <hacavanagh@gmail.com>
2021-04-01 16:07:28 +0100

    Setting base URL for API requests dynamically from manifest

--- assets/common/baseUrl.js
+++ assets/common/baseUrl.js
@@ -1,11 +1,12 @@
 import { Platform } from 'react-native'
+import Constants from 'expo-constants'
 
 
-let baseURL;
+const baseURL = `http://${Constants.manifest.debuggerHost.split(':').shift()}:3000`
 
-{Platform.OS == 'android'
-? baseURL = 'http://10.0.2.2:3000/api/v1/'
-: baseURL = 'http://localhost:3000/api/v1/'
-}
+// {Platform.OS == 'android'
+// ? baseURL = 'http://10.0.2.2:3000/api/v1/'
+// : baseURL = 'http://localhost:3000/api/v1/'
+// }
 
 export default baseURL;
