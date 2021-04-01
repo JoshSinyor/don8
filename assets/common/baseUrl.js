3e4e1e0806d6abdb57c04b4f7c721492ebb5ce3f
hacaravan <hacavanagh@gmail.com>
2021-04-01 16:41:26 +0100

    Updating base URL to be whole API string

--- assets/common/baseUrl.js
+++ assets/common/baseUrl.js
@@ -2,7 +2,7 @@ import { Platform } from 'react-native'
 import Constants from 'expo-constants'
 
 
-const baseURL = `http://${Constants.manifest.debuggerHost.split(':').shift()}:3000`
+const baseURL = `http://${Constants.manifest.debuggerHost.split(':').shift()}:3000/api/v1/`
 
 // {Platform.OS == 'android'
 // ? baseURL = 'http://10.0.2.2:3000/api/v1/'
